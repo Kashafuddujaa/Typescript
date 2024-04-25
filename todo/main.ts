507bed4f97cce9313ec6d2fe9c0840d612ef4436
Avanger <kashafudduja994@gmail.com>
2024-04-25 08:24:44 +0500

    A todo list using inquirer loops and arrays

---
+++ todo/main.ts
@@ -0,0 +1,27 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+
+while (condition)
+
+{let todoQuestions = await inquirer.prompt([
+        {
+          name: "firstQuestion",
+          type: "input",
+          message: "What would you like to add in your todos?",
+        },
+        {
+          name: "secondQuestion",
+          type: "confirm",
+          message: "Do you want to add  another task?",
+          default: "true",
+        },
+      ]);
+      
+      todos.push(todoQuestions.firstQuestion);
+      console.log(todos);
+      condition = todoQuestions.secondQuestion;
+      }
+
